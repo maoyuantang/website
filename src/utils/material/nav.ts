d8fc2bf9ad2597c270838d976585016dc6eeb832
[tangmaoyuan] <[1028278792@qq.com]>
2019-12-25 17:00:49 +0800

    nav菜单

---
+++ src/utils/material/nav.ts
@@ -0,0 +1,33 @@
+/*
+ * @Author: tmy
+ * @Date: 2019-12-25 16:12:26
+ * @LastEditors  : tmy
+ * @LastEditTime : 2019-12-25 16:18:34
+ * @Description: Do not edit
+ */
+export const navList = [
+    {
+        name: '首页',
+        path: '/home',
+    },
+    {
+        name: '解决方案',
+        path: '/Solution',
+    },
+    {
+        name: '关于我们',
+        path: '/AboutUs',
+    },
+    {
+        name: '合作案例',
+        path: '/CooperationCase',
+    },
+    {
+        name: '渠道合作',
+        path: '/ChannelCooperation',
+    },
+    {
+        name: '淘宝旗舰店',
+        path: '/TaoBaoStore',
+    },
+];
